2a84bc87d4b7781b9e8f7ce622194fe29b0573c0
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-10-05 01:15:26 +0530

    UI issue fixed

--- cbc-frontend-batch-five/src/pages/register.jsx
+++ cbc-frontend-batch-five/src/pages/register.jsx
@@ -49,12 +49,12 @@ function Register() {
   return (
     <div className='w-full h-screen bg-[url("/login.jpg")] bg-center bg-cover flex justify-center items-center'>
       <div className="w-[50%] h-full hidden md:block"></div>
-      <div className="md:w-[50%] h-[300px] flex justify-center items-center">
+      <div className="md:w-[50%] flex justify-center items-center">
         <div className='w-[300px] md:w-[500px] h-[400px] md:h-[550px] backdrop-blur-sm rounded-[30px] shadow-xl flex flex-col justify-center items-center'>
-          <input onChange={(e) => { setFirstName(e.target.value) }} value={firstName} type="text" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none' placeholder='First Name' />
-          <input onChange={(e) => { setLastName(e.target.value) }} value={lastName} type="text" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none' placeholder='Last Name' />
-          <input onChange={(e) => { setEmail(e.target.value) }} value={email} type="email" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none' placeholder='Email' />
-          <input onChange={(e) => { setPassword(e.target.value) }} value={password} type="password" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none' placeholder='Password' />
+          <input onChange={(e) => { setFirstName(e.target.value) }} value={firstName} type="text" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none w-[250px] md:w-auto' placeholder='First Name' />
+          <input onChange={(e) => { setLastName(e.target.value) }} value={lastName} type="text" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none w-[250px] md:w-auto' placeholder='Last Name' />
+          <input onChange={(e) => { setEmail(e.target.value) }} value={email} type="email" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none w-[250px] md:w-auto' placeholder='Email' />
+          <input onChange={(e) => { setPassword(e.target.value) }} value={password} type="password" className='border-b-3 border-white text-xl md:text-2xl m-[10px] p-[5px] focus:outline-none w-[250px] md:w-auto' placeholder='Password' />
           <button type="submit" onClick={handleRegister} className='rounded-[10px] cursor-pointer p-[10px] w-[120px] border-3 hover:border-white  bg-blue-100 border-blue-100 hover:bg-transparent text-gray-700 text-xl m-7 focus:outline-none font-extrabold  tracking-wider'>Register</button>
         </div>
       </div>
